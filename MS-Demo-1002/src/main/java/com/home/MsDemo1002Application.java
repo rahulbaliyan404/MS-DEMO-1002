b5355cdfe050feb4583f3790ba9b371dc55dc900
rahulbaliyan <rahulbaliyan404@gmail.com>
2021-02-18 19:05:12 +0530

    This is second commit in MS-Demo-1002.

--- MS-Demo-1002/src/main/java/com/home/MsDemo1002Application.java
+++ MS-Demo-1002/src/main/java/com/home/MsDemo1002Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MsDemo1002Application {
-
+// This is MS-Demo-1002 application main method.
 	public static void main(String[] args) {
 		SpringApplication.run(MsDemo1002Application.class, args);
 	}
